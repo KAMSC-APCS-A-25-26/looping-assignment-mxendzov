f28599a6fb2ea9391c818a6637d5f3bc358efa62
mxendzov <mxendzov17@gmail.com>
2025-10-29 11:13:28 -0400

    commmit

--- src/main/java/CharacterPerLine.java
+++ src/main/java/CharacterPerLine.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class CharacterPerLine {
     public static void main(String[] args) {        
         // TODO: Step 1 - Get user input
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String word = sc.nextLine();
+
+        for (int i = 0; i < word.length(); i++ ) {
+            System.out.println(word.charAt(i));
+        }
 
         // TODO: Step 2 - Print each character on a separate line
 
--- src/main/java/NumberTrianglePattern.java
+++ src/main/java/NumberTrianglePattern.java
@@ -4,9 +4,18 @@ public class NumberTrianglePattern {
     public static void main(String[] args) {
                 
         // TODO: Step 1 - Get user input
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = sc.nextInt();
         
         // TODO: Step 2 - Generate the number triangle pattern
-
+        for (int i = 1; i <= number; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.println(j);
+                if (j < i) {
+                    System.out.print(" ");
+        }
     }
-}
+}}}
